e24e1c1e3da9715d9ca9d3a20f853a12b98173d0
Arif Khan <arif.bannehasan@gmail.com>
2022-10-24 15:42:37 +0530

    Adding payslip response model

--- Payroll.Domain/Models/PayslipResponse.cs
+++ Payroll.Domain/Models/PayslipResponse.cs
@@ -1,6 +1,12 @@
 ﻿namespace Payroll.Domain.Models
 {
-    public class PayslipResponse
+    public record PayslipResponse
     {
+        public string? Name { get; set; }
+        public string? PayPeriod { get; set; }
+        public decimal GrossIncome { get; set; }
+        public decimal IncomeTax { get; set; }
+        public decimal NetIncome { get; set; }
+        public decimal Super { get; set; }
     }
 }
