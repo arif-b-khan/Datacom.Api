cabc7285242807556cde8b425f7b68bae51bd6a7
Arif Khan <arif.bannehasan@gmail.com>
2022-10-24 12:52:16 +0530

    Adding custom Validator
    #### Description
    - Adding minimum value validator

---
+++ Payroll/Validators/MinimumValueAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Payroll.Validators
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)] 
+    public class MinimumValueAttribute : ValidationAttribute
+    {
+        public int Minimum { get; private set; }
+        public MinimumValueAttribute(int minimum)
+        {
+            Minimum = minimum;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null)
+                return false;
+
+            if(int.TryParse(value.ToString(), out int minimumValue))
+            {
+                if(Minimum >= minimumValue)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
