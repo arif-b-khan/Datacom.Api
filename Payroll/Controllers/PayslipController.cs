49ae3a2f09f3af92026a1c78484d30505769e7d5
Arif Khan <arif.bannehasan@gmail.com>
2022-10-24 12:53:02 +0530

    Adding payslip controller
    #### Description
    - Adding payslip controller class

---
+++ Payroll/Controllers/PayslipController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Payroll.Models;
+
+namespace Payroll.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayslipController : ControllerBase
+    {
+        private readonly ILogger<PayslipController> _logger;
+        public PayslipController(ILogger<PayslipController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]PayslipRequestModel request)
+        {
+            _logger.LogInformation(request.ToString());
+            return Ok(request);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]PayslipRequestModel request)
+        {
+            _logger.LogInformation(request.ToString());
+            return Ok(request);
+        }
+    }
+}
